fe4ba8b5e2929fb26abb9ec0f009509534764930
Piotr Kawula <kontakt@piotrkawula.pl>
2023-03-11 15:48:43 +0100

    Setup msw

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,3 @@
+import type { RestHandler } from "msw";
+
+export const handlers: RestHandler[] = [];
---
+++ src/mocks/index.ts
@@ -0,0 +1,9 @@
+import { isServer } from "@/utils/helpers";
+import { worker } from "./browser";
+import { server } from "./server";
+
+if (isServer) {
+	server.listen({ onUnhandledRequest: "bypass" });
+} else {
+	worker.start({ onUnhandledRequest: "bypass" });
+}
